e86ec8ee6160e0a2b8e2d7f099f0fde531cb2ed6
Jinyoung Choi <54098630+jinyoung5497@users.noreply.github.com>
2024-09-25 11:32:12 +0900

    [Silver II] Title: 알파벳 블록, Time: 852 ms, Memory: 56904 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.py
@@ -0,0 +1,25 @@
+from sys import stdin
+from collections import deque
+now = deque()
+n = int(stdin.readline())
+stack = []
+for _ in range(n):
+    op = list(map(str, stdin.readline().split()))
+    if op[0] == '1':
+        now.append(op[1])
+        stack.append('back')
+    elif op[0] == '2':
+        now.appendleft(op[1])
+        stack.append('front')
+    else:
+        if stack:
+            value = stack.pop()
+            if value == 'back':
+                now.pop()
+            else:
+                now.popleft()
+
+if len(now) == 0:
+    print('0')
+else:
+    print("".join(now))
