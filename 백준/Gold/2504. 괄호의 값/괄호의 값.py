dce3f572c0fc811d77fc2b34fbe50e7dbf628d12
Jinyoung Choi <54098630+jinyoung5497@users.noreply.github.com>
2024-09-25 16:18:59 +0900

    [Gold V] Title: 괄호의 값, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,34 @@
+str = list(input())
+stack = []
+
+answer = 0
+temp = 1
+
+for i in range(len(str)):
+    if str[i] == '(':
+        stack.append(str[i])
+        temp *= 2
+    elif str[i] == '[':
+        stack.append(str[i])
+        temp *= 3
+    elif str[i] == ')':
+        if not stack or stack[-1] == '[':
+            answer = 0
+            break
+        if str[i - 1] == '(':
+            answer += temp
+        stack.pop()
+        temp //= 2
+    elif str[i] == ']':
+        if not stack or stack[-1] == '(':
+            answer = 0
+            break
+        if str[i - 1] == '[':
+            answer += temp
+        stack.pop()
+        temp //= 3
+
+if not stack:
+    print(answer)
+else:
+    print(0)
