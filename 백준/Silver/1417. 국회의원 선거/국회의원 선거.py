6f445cf31ff78b1680449fb0b34cfa0eb76f3ce6
Jinyoung Choi <54098630+jinyoung5497@users.noreply.github.com>
2024-09-27 13:54:40 +0900

    [Silver V] Title: 국회의원 선거, Time: 40 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -0,0 +1,23 @@
+import heapq
+
+num = int(input())
+count = 0
+heap = []
+my_votes = 0
+for i in range(num):
+    votes = int(input())
+    if i == 0:
+        my_votes = votes
+        continue
+    heapq.heappush(heap, -votes)
+
+while heap:
+    votes = -heapq.heappop(heap)
+    if votes < my_votes:
+        break
+    my_votes += 1
+    count += 1
+    heapq.heappush(heap, -(votes - 1))
+
+print(count)
+
