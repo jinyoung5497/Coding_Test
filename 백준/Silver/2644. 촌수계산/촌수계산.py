2900ba5fef7357f97eddc7bc1177492bf71128cb
Jinyoung Choi <54098630+jinyoung5497@users.noreply.github.com>
2024-10-05 16:35:00 +0900

    [Silver II] Title: 촌수계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+n = int(stdin.readline())
+x, y = map(int, stdin.readline().split())
+m = int(stdin.readline())
+relationship = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, stdin.readline().split())
+    relationship[a].append(b)
+    relationship[b].append(a)
+
+for i in relationship:
+    i.sort()
+
+def dfs(x, y, relationship, visited, count):
+    visited[x] = True
+    for i in relationship[x]:
+        if not visited[i]:
+            if i == y:
+                return count + 1
+            result = dfs(i, y, relationship, visited, count + 1)
+            if result != -1:
+                return result
+    return -1
+
+
+print(dfs(x, y, relationship, visited, 0))
+
