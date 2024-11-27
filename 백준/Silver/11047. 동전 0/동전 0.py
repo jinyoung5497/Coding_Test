6d7bb10dd3deb83673715dcda599a6d9ac3508e1
Jinyoung Choi <54098630+jinyoung5497@users.noreply.github.com>
2024-11-27 21:43:12 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,19 @@
+from sys import stdin
+input = stdin.readline
+
+n, k = map(int, input().split())
+coins = []
+for i in range(n):
+    coin = int(input())
+    coins.append(coin)
+
+reversed_coins = sorted(coins, reverse=True)
+count = 0
+for i in reversed_coins:
+    if k == 0:
+        break
+    if i <= k:
+        count += k // i
+        k -= i * (k // i)
+
+print(count)
